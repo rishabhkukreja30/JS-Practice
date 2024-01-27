fd176ec0ea50e99856149f5f2d985d4ff6fc3852
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-01-27 18:37:33 +0530

    javascript practice

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log(a);
+
+var a = 5
+
+function x() {
+  console.log("Hi, I am x");
+}
+
+setTimeout(x, 5000);
+
+
+const arr = [1,5,7,8,4]
+
+const output = arr.map(x => x*2)
+
+console.log(output);
+
+console.log(arr.filter(x => x <=7))
